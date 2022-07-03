ff0d746d6094eca92babb8646d8822075fc5a540
kristopherb.tucker <ktucker8.kt@gmail.com>
2022-07-03 13:34:14 -0400

    added all items to run application locally and in the docker containers added spring configurations for job runner configurations added image for app to work with job runner added Basic job Service to configurations added NotificationController with test implementation for verify jobs and dashboard operations added configuration guides to README.md for more precise configs.

---
+++ src/main/java/calendar/api/service/JobService.java
@@ -0,0 +1,29 @@
+package calendar.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jobrunr.jobs.annotations.Job;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class JobService {
+
+    @Job(name = "Test Job")
+    public void execute() {
+        execute("Hello world!");
+    }
+
+    @Job(name = "Event")
+    public void execute(String input) {
+        log.info("The event has begun: {}", input);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            log.error("Error while executing event", e);
+        } finally {
+            log.info("Event has finished...");
+        }
+    }
+}
